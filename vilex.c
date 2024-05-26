e67ddfa0be769544962efe0b0029d826b88d658c
SRUTHI <128819070+sruthiveni@users.noreply.github.com>
2024-05-26 18:43:02 +0530

    Create vilex.c

---
+++ vilex.c
@@ -0,0 +1,17 @@
+main()
+{
+int a,b,c,i;
+a=10;
+b=20;
+c=a+b*10;
+if(a&gt;b)
+printf(&quot;%d&quot;,a);
+else
+printf(&quot;%d&quot;,b);
+i=0;
+while(i&lt;c)
+{
+printf(&quot;%&quot;,i);
+i=i+100;
+}
+}
